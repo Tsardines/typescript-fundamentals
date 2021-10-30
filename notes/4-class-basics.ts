673cc894b6426b7e693df1c41f2368eed90c84df
Haley <hjsulcer@gmail.com>
2021-10-30 14:37:36 -0400

    Modifications to 4-class-basics.ts

--- notes/4-class-basics.ts
+++ notes/4-class-basics.ts
@@ -11,7 +11,7 @@ import { HasPhoneNumber, HasEmail } from "./1-basics";
 
 export class Contact implements HasEmail { // Implements (introduced by TS) describes a class aligning w/ a particular interface
   // Go to 1-basics.ts L133 (export interface HasEmail)
-  // We know that tyis type requires that everything have a name (str) and an email (str)
+  // We know that this type requires that everything have a name (str) and an email (str)
   // Need to make sure that these properties that conform to that interf are available stated up front ("these member data fields will exist")
   email: string; 
   name: string;
@@ -66,14 +66,18 @@ class ParamPropContact implements HasEmail {
 class OtherContact implements HasEmail, HasPhoneNumber {
   protected age: number = 0; // 1. num = 0 is a "default" value
   // You can also do: readonly age = 0 // TS will yell at you when you attempt to write to it
-  // private password: string;
+  private password!: string;
+  // The ! means that this is a "definite assignment operator"
+  // As in, "Trust me TS, I'm making sure that this field gets initialized properly...don't cause an err at this point in the code"
   constructor(
       public name: string, 
       public email: string, 
       public phone: number) {
     // 2. Doing this.age = 35 means that L67 will never happen
     // () password must either be initialized like this, or have a default value
-    // this.password = Math.round(Math.random() * 1e14).toString(32);
+    if (phone > 0) {
+      this.password = Math.round(Math.random() * 1e14).toString(32);
+    }
   }
 }
 
@@ -83,32 +87,35 @@ class OtherContact implements HasEmail, HasPhoneNumber {
  * @Five (5) TypeScript even allows for abstract classes, which have a partial implementation
  */
 
-// abstract class AbstractContact implements HasEmail, HasPhoneNumber {
-//   public abstract phone: number; // must be implemented by non-abstract subclasses
+// Abstract classes cannot be instantiated directly (they just serve as base classes)
+// Unlike an interface (which also can't be instantiated), abstr classes can have implementations
 
-//   constructor(
-//     public name: string,
-//     public email: string // must be public to satisfy HasEmail
-//   ) {}
+abstract class AbstractContact implements HasEmail, HasPhoneNumber {
+  public abstract phone: number; // must be implemented by non-abstract subclasses
 
-//   abstract sendEmail(): void; // must be implemented by non-abstract subclasses
-// }
+  constructor(
+    public name: string,
+    public email: string // must be public to satisfy HasEmail
+  ) {}
+
+  abstract sendEmail(): void; // must be implemented by non-abstract subclasses
+}
 
 
 
 /**
  * @Six (6) implementors must "fill in" any abstract methods or properties
  */
-// class ConcreteContact extends AbstractContact {
-//   constructor(
-//     public phone: number, // must happen before non property-parameter arguments
-//     name: string,
-//     email: string
-//   ) {
-//     super(name, email);
-//   }
-//   sendEmail() {
-//     // mandatory!
-//     console.log("sending an email");
-//   }
-// }
+class ConcreteContact extends AbstractContact {
+  constructor(
+    public phone: number, // must happen before non property-parameter arguments
+    name: string,
+    email: string
+  ) {
+    super(name, email);
+  }
+  sendEmail() {
+    // mandatory!
+    console.log("sending an email");
+  }
+}
